5ee0324d63097b8ff7aafdd42d57c31df5257f0b
rutvikpande <69315916+rutvikpande@users.noreply.github.com>
2023-07-29 11:04:11 +0530

    Added EMS

---
+++ EMS/employeemanager/src/main/java/com/ems/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,14 @@
+package com.ems.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class EmployeemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+}
